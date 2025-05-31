44c41ca57c6f3df642cee297ea33cff84538a996
Andres Mauricio Cano Causil <andrescaza246@hotmail.com>
2025-05-31 02:20:24 -0500

    feat(logs): Logs implemented

--- sales_system/settings.py
+++ sales_system/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
 import os
 from pathlib import Path
+import logging
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,6 +128,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 #REST_FRAMEWORK = {
 #    'DEFAULT_RENDERER_CLASSES': [
